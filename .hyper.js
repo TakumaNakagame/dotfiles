a654994bd56696276ea7dde6dca3b7f0890220cf
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2019-05-11 01:53:35 +0900

    upate hyper config

--- .hyper.js
+++ .hyper.js
@@ -4,22 +4,15 @@
 
 module.exports = {
   config: {
-
-    pokemon: ['random', 'pikachu'],
-    pokecursor: 'false',
-    pokemonSyntax: 'dark',
-    unibody: 'false',
-    poketab: 'true',
-
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 15,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'MyricaM, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
@@ -27,14 +20,8 @@ module.exports = {
     // font weight for bold characters: 'normal' or 'bold'
     fontWeightBold: 'bold',
 
-    // line height as a relative unit
-    lineHeight: 1,
-
-    // letter spacing as a relative unit
-    letterSpacing: 0,
-
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: 'rgba(253,239,242,0.5)',
 
     // terminal text color under BLOCK cursor
     cursorAccentColor: '#000',
@@ -46,20 +33,30 @@ module.exports = {
     cursorBlink: false,
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: '#dcdcdc',
 
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: '#000',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
 
     // terminal selection color
-    selectionColor: 'rgba(248,28,229,0.3)',
+    selectionColor: 'rgba(253,239,242,0.5)',
 
     // border color (window, tabs)
     borderColor: '#333',
 
     // custom CSS to embed in the main window
-    css: '',
+    css: `
+    .terms_terms{
+      background-image:url("file://C:/Users/Takuma/AppData/Roaming/Hyper/hyper-bg.png");
+      background-size: cover;
+      background-position: right top;
+      background-repeat: no-repeat;
+    };
+    .terms_termGroup {
+      background: rgba(0,0,0,0.7) !important;
+    }
+    `,
 
     // custom CSS to embed in the terminal window
     termCSS: '',
@@ -71,10 +68,10 @@ module.exports = {
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+    showWindowControls: 'false',
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '0px 14px 12px 14px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -110,11 +107,11 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+    shell: 'C:\\Windows\\System32\\wsl.exe',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
-    shellArgs: ['--login'],
+    shellArgs: ['~'],
 
     // for environment variables
     env: {},
@@ -123,7 +120,7 @@ module.exports = {
     bell: 'SOUND',
 
     // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
+    copyOnSelect: true,
 
     // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
     defaultSSHApp: true,
@@ -136,6 +133,14 @@ module.exports = {
     // bellSoundURL: 'http://example.com/bell.mp3',
 
     // for advanced config flags please refer to https://hyper.is/#cfg
+
+    // background images
+    // css: '
+    //   .terms_terms{
+    //     background:url(<span class="type">file</span>:path/hyper-bg.png)
+    //   }
+    // '
+
   },
 
   // a list of plugins to fetch and install from npm
@@ -144,11 +149,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    'hyper-pokemon',
-    'hyper-opacity',
-    //'hyper-statusline',
-  ],
+  plugins: [],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
